7fafeb4df5e6dc0d82c14d3cf80b00a62d5744f1
nirewen <londero.edu@gmail.com>
2023-08-18 18:38:05 +0000

    feat: move cron expression to env

--- apps/api/src/base/university/ufsm/providers/cron.service.ts
+++ apps/api/src/base/university/ufsm/providers/cron.service.ts
@@ -1,6 +1,9 @@
+import { Provider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { CRON_EXPRESSION } from 'src/common/providers/scheduling.service'
 
 export const CronService = {
   provide: CRON_EXPRESSION,
-  useValue: '*/5 * * * * *',
-}
+  useFactory: (config: ConfigService) => config.get('UFSM_CRON_EXPRESSION'),
+  inject: [ConfigService],
+} satisfies Provider
