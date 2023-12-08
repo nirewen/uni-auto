417a92ac6decd44eb2a289acddb37edef6524e5a
nirewen <londero.edu@gmail.com>
2023-12-08 21:14:24 +0000

    refactor: remove loki transport

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,41 +1,11 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { NestFactory, Reflector } from '@nestjs/core'
-import { WinstonModule } from 'nest-winston'
-import * as winston from 'winston'
 
 import { AppModule } from './app.module'
 import { JwtAuthGuard } from './auth/guards'
 
-const LokiTransport = require('winston-loki')
-
 async function bootstrap() {
-  const instance = winston.createLogger({
-    transports: [
-      new winston.transports.Console({
-        format: winston.format.printf(
-          ({ level, message }) => `${level}: ${message}`
-        ),
-      }),
-      new LokiTransport({
-        host: 'http://loki-loki-1:3100',
-        json: true,
-        labels: {
-          job: 'uni-auto-api',
-        },
-        format: winston.format.printf(
-          ({ level, message }) => `${level}: ${message}`
-        ),
-        batching: false,
-        replaceTimestamp: true,
-      }),
-    ],
-  })
-
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      instance,
-    }),
-  })
+  const app = await NestFactory.create(AppModule)
 
   app.setGlobalPrefix('/api')
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
