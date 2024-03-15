bbf9d2bf984b56d6a9fe7eeb7cbe02a3849ac305
nirewen <londero.edu@gmail.com>
2024-03-15 03:15:52 +0000

    temp: home -> connections

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,13 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Navigate, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+  return <Navigate to="/connections" />
 }
